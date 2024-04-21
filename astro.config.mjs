ea50ea6bdcd133e4bff5a8701b642bf52a916104
Yani Lathouris <ylathouris@gmail.com>
2024-04-22 07:09:27 +1000

    Update docs url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ylathouris.github.io",
+  site: "https://ebbyl.github.io",
   base: "ebb",
   integrations: [
     starlight({
